d38bc82efa70806f553e23c17406442fdf1c6416
Estef99RS <65564000+Estef99RS@users.noreply.github.com>
2020-06-08 15:29:02 -0500

    Update MiTercerPrograma.py

--- MiTercerPrograma.py
+++ MiTercerPrograma.py
@@ -7,7 +7,7 @@ def sumatorio(num):
 
 
 name = input("Por favor, introduce tu nombre: ")
-print("Hola " + name)
+print("Hola " + name + "!")
 num1 = int(input("Introduce un número: "))
 num2 = int(input("Introduce otro número: "))
 result = num1 * num2
